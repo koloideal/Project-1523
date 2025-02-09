1d9109a3bd19c6c0140bc70143406916ae71a068
MeatPine <metra.cosedin@bk.ru>
2025-02-09 21:50:16 +0300

    Update config.py

--- aiogram_sqla_sample-main/config.py
+++ aiogram_sqla_sample-main/config.py
@@ -2,4 +2,3 @@ TOKEN=''
 DB_URL='sqlite+aiosqlite:///db.sqlite3'
 #DB_URL='postgresql+asyncpg://user:password@host:port/dbname[?key=value&key=value...]'
 
-# Внимание! Добавьте config.py в файл .gitignore!
